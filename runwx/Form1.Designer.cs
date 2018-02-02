49eae1d42cb298810d09e0265ab21eef927ab22e
shiroo <xkq722@qq.com>
2018-02-02 16:34:10 +0800

    添加项目文件。

---
+++ runwx/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace runwx
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(310, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "开始起飞";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(84, 10);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(220, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(85, 63);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(55, 21);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "微信路径：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "启动数量：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(147, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(233, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "（受人品影响，有可能你启动并非这么多）";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(84, 38);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(173, 12);
+            this.linkLabel1.TabIndex = 6;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "微信路径不知道怎么找？点这里";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(402, 106);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "微信PC端多开助手 V1.1（by：shiroo）";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ runwx/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
+using System.Xml;
+using System.IO;
+
+namespace runwx
+{
+    public partial class Form1 : Form
+    {
+        public string configFilePath;
+        public string wxPath="";
+        public string launchesNum="5";
+        public XmlDocument XMLDoc;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //把配置文件路径存储在变量里面
+            configFilePath = Application.StartupPath + "\\config.xml";
+            XMLDoc = new XmlDocument();
+            if (!File.Exists(configFilePath))
+            {
+                //如果不存在，则创建该文件。
+                //File.Create(configFilePath);
+                //建立根节点
+                XmlElement config = XMLDoc.CreateElement("config");
+                XMLDoc.AppendChild(config);
+                //写入配置信息,变量在声明的时候已经初始化
+                config.SetAttribute("wxpath", wxPath);
+                config.SetAttribute("launchesnum", launchesNum);
+                XMLDoc.Save("config.xml");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //计算机\HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\WeChat
+            wxPath = textBox1.Text;//从编辑框里获取填写的微信路径
+            wxPath = wxPath.Replace("\"", "");//替换掉路径里的双引号
+            launchesNum = textBox2.Text;//编辑框里的启动数量
+
+            //判断微信程序路径是否正确
+            if (!File.Exists(wxPath))
+            {
+                MessageBox.Show("填写的微信路径不正确。","错误");
+                return;
+            }
+
+            XMLDoc.Load("config.xml");
+            //选择节点
+            XmlElement config = (XmlElement)XMLDoc.SelectSingleNode("config");
+            //写入配置信息
+            config.SetAttribute("wxpath", wxPath);
+            config.SetAttribute("launchesnum", launchesNum);
+            XMLDoc.Save("config.xml");
+
+            int n = Int16.Parse(launchesNum);
+            for (int i = 0; i < n; i++)
+            {
+                new Thread(RunWeiXin).Start();
+            }
+
+        }
+        public void RunWeiXin()
+        {
+            //TimeSpan tss = DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, 0);
+            //Console.WriteLine(tss.TotalMilliseconds);
+            Process.Start(wxPath);
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://zhidao.baidu.com/question/497868258393934324.html");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //读入配置文件数据
+            XMLDoc.Load("config.xml");
+            //选择节点，"config/xxxxx/xxx"
+            XmlElement config = (XmlElement)XMLDoc.SelectSingleNode("config");
+            //取得节点里的属性值
+            wxPath = config.GetAttribute("wxpath");
+            launchesNum = config.GetAttribute("launchesnum");
+            //赋值到编辑框里，展示给用户看。
+            textBox1.Text = wxPath;
+            textBox2.Text = launchesNum;
+        }
+    }
+}
